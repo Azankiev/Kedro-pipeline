81cd844ac6dd444f3255416da29059847256b9cb
Lucas Fentanes Machadou <lucas.machado@datarain.com.br>
2023-07-06 20:47:26 +0000

    feat: add node to merge partitions & add intermediate datasets

--- src/cost_pipeline/pipelines/data_engineering/nodes.py
+++ src/cost_pipeline/pipelines/data_engineering/nodes.py
@@ -18,21 +18,18 @@ def preprocess_accounts_per_org(accounts_per_org: pd.DataFrame) -> pd.DataFrame:
 
     return df
 
-def _lazy_add_account_names(cur_loader: Callable([], pd.DataFrame), df_accounts_per_org: pd.DataFrame) -> pd.DataFrame:
+def add_account_names(df_cur: pd.DataFrame, df_accounts_per_org: pd.DataFrame) -> pd.DataFrame:
 
-    df_cur = cur_loader()
-    df_cur = df_cur.merge(df_accounts_per_org, how='left', left_on='bill_payer_account_id', validate='many_to_one')
-    df_cur = df_cur.merge(df_accounts_per_org, how='left', left_on='line_item_usage_account_id', validate='many_to_one')
+    df_cur = df_cur.merge(df_accounts_per_org, how='left', left_on='bill_payer_account_id', right_on='account_id', validate='many_to_one')
+    df_cur = df_cur.merge(df_accounts_per_org, how='left', left_on='line_item_usage_account_id', right_on='account_id', validate='many_to_one')
 
     return df_cur
 
-def add_account_names(cur_dataset: Dict[str, Callable[[], Any]], df_accounts_per_org: pd.DataFrame) -> Dict[str, Callable[[], Any]]:
+def merge_cur_partitions(cur_dataset: Dict[str, Callable[[], Any]]) -> pd.DataFrame:
 
-    all_files = list(cur_dataset.items())
-    logger.info(f'Found {all_files.shape[0]} to load.')
-
-    return {
-        fname: lambda vars=[cur_loader, df_accounts_per_org]: _lazy_add_account_names(vars[0], vars[1]) 
-            for fname, cur_loader in all_files
-    } 
-
+    loaded_partitions = []
+    for filename, fileloader in cur_dataset.items():
+        logger.info(f'Appending partition {filename} to merged file...')
+        loaded_partitions.append(fileloader())
+    
+    return pd.concat(loaded_partitions)
--- src/cost_pipeline/pipelines/data_engineering/pipeline.py
+++ src/cost_pipeline/pipelines/data_engineering/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.18.10
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import preprocess_accounts_per_org, add_account_names
+from .nodes import preprocess_accounts_per_org, merge_cur_partitions, add_account_names
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -15,8 +15,16 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs='processed_accounts_per_organization'
         ),
         node(
+            func=merge_cur_partitions,
+            inputs='infra2_cur_dataset',
+            outputs='infra2_cur_dataset_merged',
+            tags=['de_infra2', 'infra2']
+        ),
+        node(
             func=add_account_names,
-            inputs=['pocs_cur_dataset', 'accounts_per_organization'],
-            outputs='pocs_cur_dataset_enriched'
+            inputs=['infra2_cur_dataset_merged', 'processed_accounts_per_organization'],
+            outputs='pocs_cur_dataset_enriched',
+            tags=['de_infra2', 'infra2']
+
         )
     ], tags='de')
